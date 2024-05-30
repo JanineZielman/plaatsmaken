1ed21f38ebadc8f84f09ead3ed7c769434671940
JanineZielman <44554453+JanineZielman@users.noreply.github.com>
2024-05-30 12:08:02 +0200

    shuffle array

--- pages/webshop.js
+++ pages/webshop.js
@@ -108,7 +108,25 @@ const Webshop = ({ navigation, settings, items }) => {
     }
   }
 
-  console.log(items)
+  function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
+
+  useEffect(() => {
+    shuffle(items)
+  }, [])
 
   
   return (
